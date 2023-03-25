0bac9910bf164a047b638190c03466c73e9095f0
Perry Bhandal <git@perryb.ca>
2023-03-25 03:16:18 -0700

    Added config flag so it knows it's a single uniform block of text.

--- score_parser.py
+++ score_parser.py
@@ -1,24 +1,41 @@
 import cv2
 import pytesseract
 
+# Set the path to the Tesseract executable file
+pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
+
 # Load the image
 img = cv2.imread("test_scoreboard.png")
 
-# Display the image and let the user select the ROI
-roi = cv2.selectROI(img)
+# Define the callback function for mouse events
+def select_roi(event, x, y, flags, params):
+    global roi, roi_gray
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # Start selecting the ROI
+        roi = (x, y)
+    elif event == cv2.EVENT_LBUTTONUP:
+        # Finish selecting the ROI
+        w = x - roi[0]
+        h = y - roi[1]
+        roi = (roi[0], roi[1], w, h)
+        roi_gray = gray[roi[1]:roi[1]+roi[3], roi[0]:roi[0]+roi[2]]
+        text = pytesseract.image_to_string(roi_gray, config='--psm 6')
+        print(text)
+        cv2.rectangle(img, roi[:2], (roi[0]+roi[2], roi[1]+roi[3]), (0, 255, 0), 2)
+        cv2.imshow("Image", img)
 
-# Extract the text within the ROI using Pytesseract
+# Set up the mouse event callback
+cv2.namedWindow("Image")
+cv2.setMouseCallback("Image", select_roi)
+
+# Convert the image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-x, y, w, h = roi
-roi_gray = gray[y:y+h, x:x+w]
-text = pytesseract.image_to_string(roi_gray)
 
-# Draw a rectangle around the ROI and display the result
-cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-cv2.imshow("Image with ROI", img)
+# Display the image
+cv2.imshow("Image", img)
+
+# Wait for a key press
 cv2.waitKey(0)
-cv2.destroyAllWindows()
 
-# Print the extracted text
-print("The extracted text is " + text)
-print(text)
+# Cleanup
+cv2.destroyAllWindows()
